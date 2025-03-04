da24b1b2b061d62de07647bfde412d14a07557a4
JoancitoGit <joan.garced@gmail.com>
2025-03-04 13:40:13 -0700

    Variables

---
+++ Variables/variables.py
@@ -0,0 +1,11 @@
+# Variables en Python
+
+#Declaración e inicialización de variables
+edad = 33
+altura = 1.74
+pais = "México"
+
+# Acceder a las variables
+print('Edad:', edad)
+print('Altura:', altura)
+print('País:', pais)
