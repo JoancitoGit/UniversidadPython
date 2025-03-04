3d280dd902f5fa3bf1c10ed65cb4a4fa0412dc81
JoancitoGit <joan.garced@gmail.com>
2025-03-04 14:13:26 -0700

    Reglas Convenciones en Variables

--- Variables/reglas_convenciones.py
+++ Variables/reglas_convenciones.py
@@ -2,3 +2,21 @@
 
 # Ejemplos de reglas estrictas
 nombre_usuario = 'Juan Perez'
+# SyntaxError: 1nombre_usuario = "Karla Gomez"
+
+# No podemos usar palabras reservadas
+# SyntaxError: class = 'Mi clase'
+klass = 'Mi Clase'
+
+# Sensibles a mayusculas y minusculas
+nombre = 'Joan'
+Nombre = 'Manuel'
+print(nombre, Nombre)
+
+# snake case
+nombre_completo = "Joan Manuel Alexander García Cedano"
+print(nombre_completo)
+
+# prefijos y sufijos
+es_casado = True
+nombre_txt = 'archivo.txt'
