26c9930991a80693fb9d0016840b33d4d37997e1
JoancitoGit <joan.garced@gmail.com>
2025-03-04 13:42:17 -0700

    Variables

--- Variables/variables.py
+++ Variables/variables.py
@@ -9,3 +9,13 @@ pais = "México"
 print('Edad:', edad)
 print('Altura:', altura)
 print('País:', pais)
+
+# Modificar el valor de una variable
+edad = 40
+altura = 2.10
+país = "USA"
+
+# Acceder a las variables
+print('Edad:', edad)
+print('Altura:', altura)
+print('País:', pais)
