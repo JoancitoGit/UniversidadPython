7fd4e6c2432ea5dafaa321874b77f591b4af5063
JoancitoGit <joan.garced@gmail.com>
2025-03-04 15:47:24 -0700

    Ejercicio tienda online

---
+++ Variables/tienda_online.py
@@ -0,0 +1,11 @@
+# Variables
+nombre_producto = "Laptop"
+precio_producto = 10000
+cantidad_inventario = 10
+disponibilidad = True
+
+print("*** PRODUCTO ***")
+print("Nombre Producto:", nombre_producto)
+print("Precio Producto:", precio_producto)
+print("Inventario:", cantidad_inventario, "Piezas")
+print("Disponible?:", disponibilidad)
