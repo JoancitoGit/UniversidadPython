02407720222614e5dbfa15ad598ccc49389cb000
JoancitoGit <joan.garced@gmail.com>
2025-03-04 14:23:41 -0700

    Ejercicio reserva_hoteles

---
+++ Variables/reserva_hoteles.py
@@ -0,0 +1,11 @@
+# Variables
+nombre_cliente = 'Joan Manuel Alexander García Cedano'
+dias_estancia = 5
+tarifa_diaria = 1200.0
+tiene_vista = True
+
+print('*** Sistema de Reserva de Hoteles ***')
+print('Cliente:', nombre_cliente)
+print ('Días de estancia:', dias_estancia)
+print('Tarifa diaria:', tarifa_diaria)
+print('Habitación con vista al mar?', tiene_vista)
