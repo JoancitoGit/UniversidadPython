e4848c680979a6c8234401b7fb3cdab013c6265b
JoancitoGit <joan.garced@gmail.com>
2025-03-04 13:46:18 -0700

    Variables

--- Variables/variables.py
+++ Variables/variables.py
@@ -6,6 +6,7 @@ altura = 1.74
 pais = "México"
 
 # Acceder a las variables
+print("VALORES INICIALES:")
 print('Edad:', edad)
 print('Altura:', altura)
 print('País:', pais)
@@ -13,9 +14,18 @@ print('País:', pais)
 # Modificar el valor de una variable
 edad = 40
 altura = 2.10
-país = "USA"
+pais = "USA"
 
 # Acceder a las variables
+print("VALORES MODIFICADOS:")
 print('Edad:', edad)
 print('Altura:', altura)
 print('País:', pais)
+
+# En Python el tipo es dinámico
+edad = 'treinta'
+print('Edad:', edad)
+
+# Si queremos acceder a una variable no declarada manda error
+telefono = '3112038196'
+print('Teléfono:', telefono)
