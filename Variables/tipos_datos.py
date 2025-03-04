cda57f1768e8e4a79700f58901b653c0b6103d95
JoancitoGit <joan.garced@gmail.com>
2025-03-04 14:21:01 -0700

    Tipos de datos

---
+++ Variables/tipos_datos.py
@@ -0,0 +1,21 @@
+# Ejemplo de tipos de datos en Python
+
+# Entero
+edad = 33
+print('Edad:', edad)
+
+# Flotante
+altura = 1.74
+print('Altura:', altura)
+
+# Cadena de texto
+nombre = 'Joan'
+print('Nombre:', nombre)
+
+# Tipo boolean
+es_estudiante = False
+print('Es estudiante?:', es_estudiante)
+
+# Tipo None, ausencia de valor
+direccion = None
+print('direccion:', direccion)
