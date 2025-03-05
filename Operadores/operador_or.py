305e77d4bee104560a73e5dff7bcff20b53b2544
JoancitoGit <joan.garced@gmail.com>
2025-03-05 13:21:57 -0700

    Operador Or

---
+++ Operadores/operador_or.py
@@ -0,0 +1,6 @@
+print('*** Operador Lógico or***')
+condicion1 = True
+condicion2 = True
+# El operador or regresa True si cualquiera de los operandos es True
+resultado = condicion1 or condicion2
+print(f'Resultado de {condicion1} or {condicion2}: {resultado}')
