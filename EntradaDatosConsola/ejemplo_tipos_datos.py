871be5cb6d1b73f541bb92e5ee729197b6518a29
JoancitoGit <joan.garced@gmail.com>
2025-03-05 09:16:51 -0700

    Ejemplo tipos datos

---
+++ EntradaDatosConsola/ejemplo_tipos_datos.py
@@ -0,0 +1,15 @@
+# Ejemplo tipos datos
+
+# Ejemplo de concatenacion o suma de valores
+numero1_cadena = '10'.
+print(f'Número 1 en cadena: {numero1_cadena}')
+numero2_cadena = '20'
+print(f'Número 2 en cadena: {numero2_cadena}')
+resultado = numero1_cadena + numero2_cadena
+print(f'Resultado: {resultado}')
+
+# Convertir a tipos enteros
+numero1 = int(numero1_cadena)
+numero2 = int(numero2_cadena)
+resultado = numero1 + numero2
+print(f'Resultado: {resultado}')
