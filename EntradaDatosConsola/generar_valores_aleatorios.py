fa790d94b8454f38267024e20738be7a64be40d6
JoancitoGit <joan.garced@gmail.com>
2025-03-05 10:26:32 -0700

    generar valores aleatorios

---
+++ EntradaDatosConsola/generar_valores_aleatorios.py
@@ -0,0 +1,10 @@
+# Valores aleatorios con la funcion randint
+#import random
+from random import randint
+
+numero_aleatorio = randint(0,10)
+print(f'Número aleatorio: {numero_aleatorio}')
+
+# Simular un dado de 6 caras
+dado = randint(1, 6)
+print(f'Resultado de lanzar el dado: {dado}')
