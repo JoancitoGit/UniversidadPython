71056d2e5e3e90d5ee37cb90b37d9172d22158b8
JoancitoGit <joan.garced@gmail.com>
2025-03-05 15:14:44 -0700

    area y perimetro de rectangulo

---
+++ Operadores/area_perimetro_rectangulo.py
@@ -0,0 +1,7 @@
+base = float(input('Ingresa la base del rectangulo: '))
+altura = float(input('Ingresa la altura del rectangulo: '))
+
+area = base * altura
+perimetro = 2 * (base + altura)
+
+print(f'El rectangulo tiene como base: {base} y altura: {altura}, su area es: {area} y su perimetro es: {perimetro}')
