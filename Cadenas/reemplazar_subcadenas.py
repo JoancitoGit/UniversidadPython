4747dfee02c757bbc79044cef4760876055b343a
ErrorExceptionSystems <error.exception.gaming@gmail.com>
2025-03-04 22:36:23 -0700

    reemplazar subcadenas

---
+++ Cadenas/reemplazar_subcadenas.py
@@ -0,0 +1,11 @@
+# Reemplazar subcadenas en Python
+
+cadena = 'Hola, mundo!'
+nueva_cadena = cadena.replace('mundo','a todos')
+
+print(nueva_cadena)
+print(cadena)
+
+# Sustituir hola por adios
+nueva_cadena = cadena.replace('Hola', 'Adios')
+print(nueva_cadena)
