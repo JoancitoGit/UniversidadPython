80120ce6a678f8ef487d506205531198acde34e8
ErrorExceptionSystems <error.exception.gaming@gmail.com>
2025-03-04 21:50:38 -0700

    caracteres especiales

---
+++ Cadenas/caracteres_especiales.py
@@ -0,0 +1,6 @@
+# Caracteres Especiales
+print('Hola \nMundo') # \n Salto de Linea
+print('\tHola Mundo') # \t Tabulador
+print('Hola \'Mundo\'') # \' Comilla Simple
+print('Hola \"Mundo\"') # \" Comilla Doble
+print('Hola \\Mundo\\') # \\ Diagonal Ivertida 
