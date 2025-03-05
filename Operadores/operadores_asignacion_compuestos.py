ca4ac6a5a56c7cf4e90aa89ee1f0f47c4c1716cd
JoancitoGit <joan.garced@gmail.com>
2025-03-05 12:42:56 -0700

    operadores de asignacion compuestos

---
+++ Operadores/operadores_asignacion_compuestos.py
@@ -0,0 +1,22 @@
+print('*** Operadores Asignacion Compuestos ***')
+a, b = 10, 15
+print(f'Valor inicial a: {a}, b: {b}')
+
+# Operador copuesto de suma +=
+a += b # a = a + b
+print(f'Operador a += b es: {a}')
+
+# Operador copuesto de resta -=
+a = 10
+a -= b # a = a - b
+print(f'Operador a -= b es: {a}')
+
+# Operador copuesto de multiplicacion *=
+a = 10
+a *= b # a = a * b
+print(f'Operador a *= b es: {a}')
+
+# Operador copuesto de division /=
+a = 10
+a /= b # a = a / b
+print(f'Operador a /= b es: {a:.2f}')
