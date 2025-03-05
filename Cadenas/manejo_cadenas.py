388e84dddd47d5722b2bb1121c526738e2ff136f
ErrorExceptionSystems <error.exception.gaming@gmail.com>
2025-03-04 21:32:01 -0700

    Manejo de Cadenas

--- Cadenas/manejo_cadenas.py
+++ Cadenas/manejo_cadenas.py
@@ -1,3 +1,11 @@
 # Cadenas en Python
 cadena1 = 'Hola Mundo'
+cadena1 = 'Adios'
+cadena2 = "Python es Genial"
+cadena3 = """Hola
+          Esta es 
+          Una cadena
+          Triple"""
 print("Cadena 1:", cadena1)
+print("Cadena 2:", cadena2)
+print("Cadena 3:", cadena3)
