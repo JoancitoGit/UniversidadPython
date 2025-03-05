0c7a4eacd11080215ffa541bd7e938bce64a0f65
JoancitoGit <joan.garced@gmail.com>
2025-03-05 11:42:09 -0700

    operadores aritmeticos

---
+++ Operadores/operadores_aritmeticos.py
@@ -0,0 +1,32 @@
+# Operadores Aritméticos
+
+a = 10
+b = 3
+
+# Suma +
+suma = a + b
+print(f"Suma a + b = {suma}")
+
+# Resta -
+resta = a - b
+print(f"Resta a - b = {resta}")
+
+# Multiplicación
+multiplicacion = a * b
+print(f"Multiplicación a * b = {multiplicacion}")
+
+# División / (retorna un tipo float)
+division = a / b
+print(f'División a / b = {division:.2f}')
+
+# División entera/ (retorna un tipo int)
+division = a // b
+print(f'División a // b = {division}')
+
+# Modulo (%) residuo de la división
+modulo = a % b
+print(f'Residuo de la división a % b = {modulo}')
+
+# Exponente (potencia) **
+exponente = a**b
+print(f'Exponente de a ** b = {exponente}')
