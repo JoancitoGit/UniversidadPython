8134eaab584785d87fc672ccd48807c4fcca64dd
ErrorExceptionSystems <error.exception.gaming@gmail.com>
2025-03-04 22:30:15 -0700

    buscar subcadenas metodo find

---
+++ Cadenas/buscar_subcadenas.py
@@ -0,0 +1,7 @@
+# Buscar subcadenas en Python
+cadena = 'Hola, mundo! mundo mundo'
+print(f'Encontrar el indice de la subcadena "mundo:" {cadena.find("mundo")}')
+
+# Obtener el indice de la subcadena de Hola
+indice = cadena.find("Hola")
+print(f'Indice de la subcadena de HOla {indice}')
