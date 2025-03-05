b198d583a1f50c414eb1697cd32949c81701aad7
JoancitoGit <joan.garced@gmail.com>
2025-03-05 08:46:44 -0700

    conversion tipos datos

---
+++ EntradaDatosConsola/conversion_tipos_datos.py
@@ -0,0 +1,14 @@
+# Conversión de tipos de datos
+
+# Convertir cadena a número
+
+numero_cadena = '10'
+numero_entero = int(numero_cadena)
+print(f'Valor númerico en cadena: {numero_cadena}')
+print(f'Cadena a entero: {numero_entero}')
+
+# Convertir de cadena a flotante
+numero_cadena = '3.14'
+numero_flotante = float(numero_cadena)
+print(f'Valor numerico en cadena: {numero_cadena}')
+print(f'Cadena a flotante: {numero_flotante}')
