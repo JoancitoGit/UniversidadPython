5ce614bfa48f31f8062ce98a2febca60b6f1631c
JoancitoGit <joan.garced@gmail.com>
2025-03-05 12:38:31 -0700

    Asignación multiple

--- Operadores/operadores_asignacion.py
+++ Operadores/operadores_asignacion.py
@@ -13,3 +13,13 @@ print(x,y,z)
 # Asignacion encadenada
 var1 = var2 = var3 = 10
 print(var1, var2, var3)
+
+# Intercambio de valores de una variable, sin utilizar variables temporales
+x, y = 5, 10
+# Aplicando el concepto de asignación multiple, intercambiamos valores
+x, y = y, x
+print(f' x = {x} y = {y}')
+
+# Recibir multiples valores de la entrada del usuario
+nombre, apellido = input('Ingresa tu nombre y apellido separados por coma: ').split(',')
+print(f'Nombre = {nombre.strip()}, Apellido = {apellido.strip()}')
