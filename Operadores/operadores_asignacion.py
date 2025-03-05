372c8c74fbf9004dc7600d0d05d0f54e94cf71d5
JoancitoGit <joan.garced@gmail.com>
2025-03-05 12:33:01 -0700

    operadores de asignacion, multiple y encadenada

--- Operadores/operadores_asignacion.py
+++ Operadores/operadores_asignacion.py
@@ -5,3 +5,11 @@ numero = 10
 print(f'Valor de numero: {numero}')
 cadena = 'Saludos desde Python'
 print(f'Valor de la cadena: {cadena}')
+
+# Asignacion multiple
+x, y, z = 5, 'Hola', -9.15
+print(x,y,z)
+
+# Asignacion encadenada
+var1 = var2 = var3 = 10
+print(var1, var2, var3)
