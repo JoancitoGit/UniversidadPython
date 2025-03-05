9989a12253f0ddf49623d7894f0d2a049ebb5d88
JoancitoGit <joan.garced@gmail.com>
2025-03-05 08:55:04 -0700

    conversion de tipos de datos

--- EntradaDatosConsola/conversion_tipos_datos.py
+++ EntradaDatosConsola/conversion_tipos_datos.py
@@ -12,3 +12,35 @@ numero_cadena = '3.14'
 numero_flotante = float(numero_cadena)
 print(f'Valor numerico en cadena: {numero_cadena}')
 print(f'Cadena a flotante: {numero_flotante}')
+
+# Convertir de numero a cadena
+numero_entero = 25
+numero_cadena = str(numero_entero)
+print(f'Valor cadena a numérico: {numero_entero}')
+print(f'Numerico a cadena: {numero_cadena}')
+
+# Convertir a booleano
+# Tipo bool es Falso en los siguientes casos
+# Si el valor es 0, cadena vacía, o None, entonces regresa False
+# Regresa True, si el valor es distinto de 0, si es distinto de cadena vacía
+# y también si es distinto de None
+
+numero_entero = 0
+booleano = bool(numero_entero)
+print(f'Valor de entero a Booleano: {booleano}')
+
+numero_entero = 5
+booleano = bool(numero_entero)
+print(f'Valor de entero a Booleano: {booleano}')
+
+cadena = ''
+booleano = bool(cadena)
+print(f'Valor de cadena a Booleano: {booleano}') # Falso, incliye colecciones vacías
+
+cadena = 'Cadena con valor'
+booleano = bool(cadena)
+print(f'Valor de cadena a Booleano: {booleano}') # True
+
+variable = None
+booleano = bool(variable)
+print(f'Valor de None a Booleano: {booleano}')
