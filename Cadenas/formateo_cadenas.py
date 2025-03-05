4a45367277b1f7d1076ad00217f108cfeb5b2ba7
ErrorExceptionSystems <error.exception.gaming@gmail.com>
2025-03-04 22:06:39 -0700

    formateo cadenas

---
+++ Cadenas/formateo_cadenas.py
@@ -0,0 +1,9 @@
+# Formateo de cadenas
+
+nombre = "Juan"
+edad = 30
+print(f'Nombre: {nombre}, Edad: {edad} años')
+
+# Método format
+mensaje = 'Hola, me llamo {} y tengo {} años'.format(nombre, edad)
+print(mensaje)
