6ea903f4a9a1b35fa59ca2c00699b1c5d0aa1ddb
ErrorExceptionSystems <error.exception.gaming@gmail.com>
2025-03-04 21:55:54 -0700

    concatenar cadenas

---
+++ Cadenas/concatenar_cadenas.py
@@ -0,0 +1,9 @@
+# Concatenar Cadenas
+print("Hola" + " " + "Mundo")
+cadena1 = 'Adios'
+cadena2 = 'A Todos'
+print(cadena1 + " " + cadena2)
+
+#Utilizando el metodo join
+concatenacion = ''.join([cadena1, ' ', cadena2])
+print(concatenacion)
