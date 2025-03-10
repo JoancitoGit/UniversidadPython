ed0a04370e9b414cae8f8d4720014c07ef55432a
joangarced <joan.garced@gmail.com>
2025-03-09 21:35:08 -0700

    menu iterativo v2

--- Ciclos/menu_iterativo.py
+++ Ciclos/menu_iterativo.py
@@ -1,15 +1,18 @@
 print(f'*** Sistema de ADministración de Cuentas ***')
 
-seleccion = 0
-while seleccion != 3:
-    print("""Menu:
-                1. Crear Cuenta
-                2. Eliminar Cuenta
-                3. Salir""")
-    seleccion = int(input('Escoje una opción: '))
-    if(seleccion == 1):
-        print('Creando tu cuenta...')
-    elif(seleccion == 2):
-        print('Eliminando tu cuenta...')
+salir = False
+while not salir:
+    print(f'''Menu:
+    1. Crear cuenta
+    2. Eliminar cuenta
+    3. Salir''')
+    opcion = int(input('Escoje una opción: '))
+    if opcion == 1:
+        print('Creando tu cuenta...\n')
+    elif opcion == 2:
+        print('Eliminando tu cuenta...\n')
+    elif opcion == 3:
+        print('Saliendo del sistema. Hasta pronto!\n')
+        salir = True
     else:
-        seleccion=0
+        print('Opción inválida, proporciona otra opción\n')
