f512963723f5ff4ffd31b2c4b8c9b7b7ef82bb9a
joangarced <joan.garced@gmail.com>
2025-03-09 21:19:24 -0700

    lista de frutas

--- Ciclos/ciclo_for.py
+++ Ciclos/ciclo_for.py
@@ -4,7 +4,7 @@ cadena = 'Hola Mundo'
 for letra in cadena:
     print(letra, end = ' ')
 
-
+print(f'\nRecorremos la lista de Frutas:')
 frutas = ['Platano', 'Fresa', 'Mango']
 for fruta in frutas:
     print(fruta, end = ' ')
