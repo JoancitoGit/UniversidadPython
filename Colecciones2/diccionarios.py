a8de25a3a6b88d21382f75011d827f460a97c7af
JoancitoGit <joan.garced@gmail.com>
2025-03-11 15:52:12 -0700

    diccionarios

---
+++ Colecciones2/diccionarios.py
@@ -0,0 +1,9 @@
+print('*** Diccionarios en Python ***')
+
+# Creamos un dict de persona con clave y valor
+persona = {
+    'nombre': 'Joan',
+    'edad': 33,
+    'ciudad': 'Tepic',
+    'profesion': 'Ingeniero'
+}
