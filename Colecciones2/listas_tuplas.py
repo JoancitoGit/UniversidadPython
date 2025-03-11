9b4f130c10d2bd9c0d27a810e182fff04667611a
JoancitoGit <joan.garced@gmail.com>
2025-03-11 12:45:34 -0700

    combinacion listas y tuplas

---
+++ Colecciones2/listas_tuplas.py
@@ -0,0 +1,8 @@
+print('*** Combinación de Listas Y Tuplas ***')
+
+# Definir una lista que almacena tuplas de productos
+productos = [
+    ('P001', 'Camiseta', 20.00),
+    ('P002', 'Jeans', 30.00),
+    ('P003', 'Sudadera', 40.00)
+]
