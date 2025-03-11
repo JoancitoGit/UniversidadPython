e231501502490079716fec6181d18b243498aa2f
joangarced <joan.garced@gmail.com>
2025-03-10 20:51:58 -0700

    listas

--- Colecciones/listas.py
+++ Colecciones/listas.py
@@ -6,4 +6,16 @@ print(f'Largo de la lista: {len(mi_lista)}')
 
 # Acceder a los elementos de la lista por indice
 print(f'Accedemos al valor del indice 4: {mi_lista[4]}')
-print(f'Accedemos al último indice de la lista: {mi_lista[-1g]}')
+print(f'Accedemos al último indice de la lista: {mi_lista[-1]}')
+
+# Modificar los elementos de una lista
+mi_lista[1] = 10
+print(f'Modificamos el valor del indice 1: {mi_lista[1]}')
+
+# Agregar un nuevo elemento al final de la lista
+mi_lista.append(6)
+print(f'La lista actual está así: {mi_lista}')
+
+# Añadir un nuevo elemento en un indice especifico
+mi_lista.insert(2, 15)
+print(f'La lista actual está así: {mi_lista}')
