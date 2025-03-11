14d7c26abefe46ce3e8401a53f0ef641e5992ae3
JoancitoGit <joan.garced@gmail.com>
2025-03-11 12:51:22 -0700

    listas con tuplas

--- Colecciones2/listas_tuplas.py
+++ Colecciones2/listas_tuplas.py
@@ -6,3 +6,15 @@ productos = [
     ('P002', 'Jeans', 30.00),
     ('P003', 'Sudadera', 40.00)
 ]
+
+# Imprimir la información de cada producto
+# Y Además calculamos el preci ototal
+
+precio_total = 0
+
+print('Informacion de los productos: ')
+for producto in productos:
+    id, descripcion, precio = producto # Unpacking
+    print(f'Producto: id = {id}, descripcion = {descripcion}, precio = ${precio}')
+    precio_total += precio # producto[2]
+print(f'Precio total de los productos: ${precio_total}')
