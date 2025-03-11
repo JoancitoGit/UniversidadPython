b5173810ccbefe25fccf36e8de95310ee8803ad9
joangarced <joan.garced@gmail.com>
2025-03-10 19:12:39 -0700

    no mms

--- Colecciones/listas.py
+++ Colecciones/listas.py
@@ -1,3 +1,9 @@
 print('*** Manejo de Listas ***')
 
 mi_lista = [1, 2, 3, 4, 5]
+print(f'{mi_lista} -> Lista original')
+print(f'Largo de la lista: {len(mi_lista)}')
+
+# Acceder a los elementos de la lista por indice
+print(f'Accedemos al valor del indice 4: {mi_lista[4]}')
+print(f'Accedemos al último indice de la lista: {mi_lista[-1g]}')
