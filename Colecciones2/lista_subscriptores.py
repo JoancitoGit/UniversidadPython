fc58436187a118a6a78b15c04391c6d8f0a33640
JoancitoGit <joan.garced@gmail.com>
2025-03-11 13:47:18 -0700

    Sistema de suscriptores con Sets

--- Colecciones2/lista_subscriptores.py
+++ Colecciones2/lista_subscriptores.py
@@ -1,10 +1,19 @@
 print('*** Lista de Suscriptores ***')
 
-suscriptores = {'luisa@gmail.com', 'marcos@gmail.com', 'elena@gmail.com'}
+# Definimos el set inicial
+# suscriptores = {} # Aqui se define un diccionario vacío
+suscriptores = set() # Definimos un set vacio
+
+numero_suscriptores = int(input('Ingresa la cantidad de suscriptores iniciales: '))
+
+for _ in range(numero_suscriptores):
+    suscriptores.add(input('Nuevo suscriptor (email): '))
+
+
 print(f'Lista de suscriptores inicial: {suscriptores}')
 
 # Verifica si un nuevo suscriptor ya está en la lista
-nuevo_suscriptor = 'alex@gmail.com'
+nuevo_suscriptor = input('Proporciona el nuevo suscriptor: ')
 if nuevo_suscriptor in suscriptores:
     print(f'El nuevo suscriptor ya está en la lista: {nuevo_suscriptor}')
 else:
@@ -14,7 +23,7 @@ else:
 print(f'Lista de suscriptores final: {suscriptores}')
 
 # Eliminamos nu suscriptor
-suscriptor_eliminar = 'elena@gmail.com'
+suscriptor_eliminar = input('Porporciona el suscriptor a eliminar: ')
 suscriptores.remove(suscriptor_eliminar)
 print(f'El suscriptor: {suscriptor_eliminar} ha sido eliminado de la lista')
 print(f'Lista de suscriptores: {suscriptores}')
