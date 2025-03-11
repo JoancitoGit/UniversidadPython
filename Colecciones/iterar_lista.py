1e450617534aae128e0cad95d206e3d49a27cb70
joangarced <joan.garced@gmail.com>
2025-03-10 21:52:31 -0700

    ejercicio playlist

---
+++ Colecciones/iterar_lista.py
@@ -0,0 +1,9 @@
+print(f'Iterar listas')
+
+nombres = ['Karla', 'Juan', 'Laura']
+for nombre in nombres:
+    print(nombre, end=' ')
+
+lista_heterogenea = [100, True, 'Ivonne']
+for item in lista_heterogenea:
+    print(f'\n{item}', end=' ')
