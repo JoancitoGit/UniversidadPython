0b397f80d5819170815e4a064696bd70e3329baa
JoancitoGit <joan.garced@gmail.com>
2025-03-11 12:25:26 -0700

    desesmpaquetado

---
+++ Colecciones2/desempaquetado.py
@@ -0,0 +1,8 @@
+print('*** Desempaquetado de Tuplas ***') # Unpacking
+
+producto = ('P001', 'Camisa', 20.00)
+
+# Desempaquetado
+id, descripcion, precio = producto
+
+print(f'id: {id}, descripcion: {descripcion}, precio: {precio}')
