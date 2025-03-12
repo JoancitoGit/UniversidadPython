70eabae598ce05e78f05e192698c8ace72f8d33d
joangarced <joan.garced@gmail.com>
2025-03-11 21:14:25 -0700

    función sumar

---
+++ Funciones/sumar.py
@@ -0,0 +1,11 @@
+print('*** Función sumar ***')
+
+def sumar(num1, num2):
+    resultado_suma = num1 + num2
+    return resultado_suma
+
+resultado_funcion = sumar(1,2)
+print(f'Resultado función sumar: {resultado_funcion}')
+
+resultado_funcion = sumar(9,15)
+print(f'Resultado función sumar: {resultado_funcion}')
