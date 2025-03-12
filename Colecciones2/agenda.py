eb0603fb224a3437fce7c07c5be9ccd6af14f6b2
joangarced <joan.garced@gmail.com>
2025-03-11 19:15:22 -0700

    ejercicio agenda

---
+++ Colecciones2/agenda.py
@@ -0,0 +1,18 @@
+print('*** Agenda de Contactos ***')
+
+agenda = {
+    'Carlos': {
+        'telefono': '3112038196',
+        'email': 'carlos@mail.com',
+        'direccion': 'Calle Principal'},
+    'María': {
+        'telefono': '322222222',
+        'email': 'maria@mail.com',
+        'direccion': 'Calle Secundaria'},
+    'Pedro': {
+        'telefono': '3111238931',
+        'email': 'pedro@mail.com',
+        'direccion': 'Calle Final'}
+}
+
+print(agenda)
