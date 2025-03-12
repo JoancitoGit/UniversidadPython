4e62f0a07c6882cdf56ed57af8ceb49521bed337
JoancitoGit <joan.garced@gmail.com>
2025-03-12 15:18:54 -0700

    Constructores con parametros flexibles

--- ClasesObjetos/aritmetica.py
+++ ClasesObjetos/aritmetica.py
@@ -1,6 +1,6 @@
 class Aritmetica:
 
-    def __init__(self, operando1, operando2):
+    def __init__(self, operando1 = None, operando2 = None):
         self.operando1 = operando1
         self.operando2 = operando2
 
@@ -23,3 +23,13 @@ if __name__ == '__main__':
 
     aritmetica2 = Aritmetica(12,16)
     print(aritmetica2.sumar())
+    #Tercer objeto
+    aritmetica3 = Aritmetica(7)
+    aritmetica3.operando2 = 9
+    print(aritmetica3.sumar())
+
+    # Cuarto Objeto
+    aritmetica4 = Aritmetica()
+    aritmetica4.operando1 = 10
+    aritmetica4.operando2 = 20
+    print(aritmetica4.sumar())
