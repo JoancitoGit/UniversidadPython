16310ab7791e1d78d0226bb0ea24bc13ee9f0cda
joangarced <joan.garced@gmail.com>
2025-03-11 19:53:16 -0700

    inventarios final

--- Colecciones2/inventarios.py
+++ Colecciones2/inventarios.py
@@ -15,3 +15,28 @@ for indice in range(numero_productos):
 
 # Mostrar el inventario inicial
 print(f'Inventario inicial:\n {inventario}')
+
+# Buscar un producto por el id
+id_buscar = int(input('\nIngresa el ID del producto a buscar: '))
+producto_encontrado = None
+for producto in inventario:
+    if producto.get('id') == id_buscar:
+        producto_encontrado = producto
+        break
+
+if producto_encontrado is not None:
+    print('Información de producto encontrado: ')
+    print(f'''Id: {producto_encontrado.get('id')},
+    Nombre: {producto_encontrado.get('nombre')},
+    Precio: {producto_encontrado.get('precio')},
+    Cantidad: {producto_encontrado.get('cantidad')}''')
+else:
+    print(f'Producto con id {id_buscar} No encontrado')
+
+# Mostrar inventario detallado
+print(f'\n--- Inventario Detallado ---')
+for producto in inventario:
+    print(f'''Id: {producto.get('id')}
+    Nombre: {producto.get('nombre')}
+    Precio: ${producto.get('precio'):.2f}
+    Cantidad: {producto.get('cantidad')}''')
