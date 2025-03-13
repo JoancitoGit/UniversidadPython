9eeea15d87b48a6bc77f6a6acdb7df30d31c4b73
joangarced <joan.garced@gmail.com>
2025-03-12 21:52:41 -0700

    Encapsulamiento buenas prácticas

--- ClasesObjetos/coche.py
+++ ClasesObjetos/coche.py
@@ -2,16 +2,34 @@
 class Coche:
 
     def __init__(self, marca, modelo, color):
-        self.marca = marca
-        self._modelo = modelo # Atributo protegido
-        self.__color = color # Atributo privado
+        self._marca = marca
+        self._modelo = modelo
+        self._color = color
 
     def conducir(self):
         print(f'''Conduciendo el coche:
-        Marca: {self.marca}
+        Marca: {self._marca}
         Modelo: {self._modelo}
-        Color: {self.__color}
+        Color: {self._color}
         ''')
+
+    def get_marca(self):
+        return self._marca
+
+    def set_marca(self, marca):
+        self._marca = marca
+
+    def get_modelo(self):
+        return self._modelo
+
+    def set_modelo(self, modelo):
+        self._modelo = modelo
+
+    def get_color(self):
+        return self._color
+
+    def set_color(self, color):
+        self._color = color
 
 # Programa principal
 
@@ -19,9 +37,8 @@ if __name__ == '__main__':
     # Creacion de un primer coche
     coche1 = Coche('Toyota', 'Yaris', 'Azul')
     coche1.conducir()
-    # No deberiamos acceder a los atibutos que no sean publicos
-    coche1.marca = 'Toyota 2'
-    coche1._modelo = 'Yaris 2' # Esto no es una buena practica
-    coche1.__color = 'Azul 2'
-    coche1._Coche__color = 'Azul 3'
+
+    coche1.set_marca('Toyota 2')
+    coche1.set_modelo('Yaris 2')
+    coche1.set_color('Azul 2')
     coche1.conducir()
