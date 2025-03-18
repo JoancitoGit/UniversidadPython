8587b3b8555ddc1036995f47c679c4ee23349c57
JoancitoGit <joan.garced@gmail.com>
2025-03-18 12:10:43 -0700

    Sistema Empleados, clase Empleado

---
+++ ClasesObjetos/empresa.py
@@ -0,0 +1,9 @@
+class Empresa:
+
+    nombre = ''
+    empleados = []
+
+    def contratar_empleado(self, nombre, departamento):
+
+
+    def obtener_numero_empleados_por_departamento(self, departamento):
