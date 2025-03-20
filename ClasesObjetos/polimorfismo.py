87306a941f4a5d5082272d6e5f2ca6584e3de382
joangarced <joan.garced@gmail.com>
2025-03-19 21:59:31 -0700

    Polimorfismo objetos

--- ClasesObjetos/polimorfismo.py
+++ ClasesObjetos/polimorfismo.py
@@ -12,3 +12,15 @@ class Gato(Animal):
     def hacer_sonido(self):
         print('Puedo Maullar')
 
+print('*** Ejemplo Polimorfismo ***')
+print('Clase Padre Animal: ')
+animal1 = Animal()
+animal1.hacer_sonido()
+
+print('Clase Hija Perro: ')
+perro1 = Perro()
+perro1.hacer_sonido()
+
+print('Clase Hija Gato: ')
+gato1 = Gato()
+gato1.hacer_sonido()
