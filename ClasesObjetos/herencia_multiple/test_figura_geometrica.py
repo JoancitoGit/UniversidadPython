e8d216b73453c9af417ca6e47c38b7ea45a625b9
JoancitoGit <joan.garced@gmail.com>
2025-03-20 11:38:49 -0700

    validaciones en app figura geometrica

--- ClasesObjetos/herencia_multiple/test_figura_geometrica.py
+++ ClasesObjetos/herencia_multiple/test_figura_geometrica.py
@@ -1,5 +1,12 @@
 from herencia_multiple.cuadrado import Cuadrado
+from herencia_multiple.rectangulo import Rectangulo
 
-cuadrado1 = Cuadrado(5, 'rojo')
+print('Creación Objeto Cuadradado'.center(50, '-'))
+cuadrado1 = Cuadrado(lado = 15, color = 'rojo')
 print(f'Cálculo área cuadrado: {cuadrado1.calcular_area()}')
 print(cuadrado1)
+
+print('Creación Objeto Rectangulo'.center(50, '-'))
+rectangulo1 = Rectangulo(ancho = 13,alto = 8, color = 'verde')
+print(f'Cálculo área rectangulo: {rectangulo1.calcular_area()}')
+print(rectangulo1)
