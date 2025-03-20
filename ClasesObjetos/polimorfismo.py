a415df5f6b8fe1912a76144c2491992a7fcedec7
joangarced <joan.garced@gmail.com>
2025-03-19 21:58:07 -0700

    Polimorfismo

---
+++ ClasesObjetos/polimorfismo.py
@@ -0,0 +1,14 @@
+class Animal:
+
+    def hacer_sonido(self):
+        print('Hago un Pitido')
+
+class Perro(Animal):
+
+    def hacer_sonido(self):
+        print('Puedo Ladrar')
+
+class Gato(Animal):
+    def hacer_sonido(self):
+        print('Puedo Maullar')
+
